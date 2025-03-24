3e4a7c3899437c6e8b91001cf8d1b4de0b679f7e
Luis Meirelles <luis.gustavo@essentia.com.br>
2025-03-23 21:53:48 -0300

    feat: implement three way handshake
    now a connection can be established and one time echo (as it is not
    acknoledged, retransmit will be triggered normally until this packet is
    dropped of) will be executed

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod tcp;
 
 use etherparse::{Ipv4HeaderSlice, TcpHeaderSlice};
-use std::{collections::HashMap, io, net::Ipv4Addr};
+use std::{
+    collections::{HashMap, hash_map::Entry},
+    io,
+    net::Ipv4Addr,
+};
 use tun_tap::{Iface, Mode};
 
-const IPV4_CODE: u16 = 0x0800;
 const TCP_PROTO: u8 = 0x06;
 
 #[derive(Hash, PartialEq, Eq, Clone, Copy, Debug)]
@@ -14,23 +17,15 @@ struct Quad {
 }
 
 fn main() -> io::Result<()> {
-    let mut connections: HashMap<Quad, tcp::State> = Default::default();
-    let interface = Iface::new("tun0", Mode::Tun)?;
+    let mut connections: HashMap<Quad, tcp::Connection> = Default::default();
+    let mut interface = Iface::without_packet_info("tun0", Mode::Tun)?;
 
     loop {
-        let mut buffer = [0u8; 1504];
+        let mut buffer = [0u8; 1500];
 
         let packet_length = interface.recv(&mut buffer)?;
 
-        let _ethernet_flags = u16::from_be_bytes([buffer[0], buffer[1]]);
-
-        let ip_version = u16::from_be_bytes([buffer[2], buffer[3]]);
-
-        if ip_version != IPV4_CODE {
-            continue;
-        }
-
-        match Ipv4HeaderSlice::from_slice(&buffer[4..packet_length]) {
+        match Ipv4HeaderSlice::from_slice(&buffer[..packet_length]) {
             Ok(ip_header) => {
                 let protocol = ip_header.protocol();
                 let protocol_code: u8 = protocol.into();
@@ -39,12 +34,12 @@ fn main() -> io::Result<()> {
                     continue;
                 }
 
-                match TcpHeaderSlice::from_slice(&buffer[4 + ip_header.slice().len()..]) {
+                match TcpHeaderSlice::from_slice(&buffer[ip_header.slice().len()..packet_length]) {
                     Ok(tcp_header) => {
                         let ip_header_size = ip_header.slice().len();
                         let tcp_header_size = tcp_header.slice().len();
 
-                        let data_offset = 4 + ip_header_size + tcp_header_size;
+                        let data_offset = ip_header_size + tcp_header_size;
 
                         let source_address = ip_header.source_addr();
                         let source_port: u16 = tcp_header.source_port();
@@ -54,13 +49,24 @@ fn main() -> io::Result<()> {
                         let destination_port: u16 = tcp_header.destination_port();
                         let destination = (destination_address, destination_port);
 
-                        connections
-                            .entry(Quad {
-                                source,
-                                destination,
-                            })
-                            .or_default()
-                            .on_packet(ip_header, tcp_header, &buffer[data_offset..]);
+                        match connections.entry(Quad {
+                            source,
+                            destination,
+                        }) {
+                            Entry::Occupied(mut connection) => connection.get_mut().on_packet(
+                                &mut interface,
+                                ip_header,
+                                tcp_header,
+                                &buffer[data_offset..packet_length],
+                            )?,
+                            Entry::Vacant(connections) => {
+                                if let Some(c) =
+                                    tcp::Connection::accept(&mut interface, ip_header, tcp_header)?
+                                {
+                                    connections.insert(c);
+                                }
+                            }
+                        }
                     }
                     Err(e) => {
                         eprintln!("skipping unknown TCP packet: {:?}", e);
--- src/tcp.rs
+++ src/tcp.rs
@@ -1,21 +1,145 @@
-use etherparse::{Ipv4HeaderSlice, TcpHeaderSlice};
+use std::io;
+
+use etherparse::{IpNumber, Ipv4Header, Ipv4HeaderSlice, TcpHeader, TcpHeaderSlice};
+use tun_tap::Iface;
+
+#[allow(dead_code)]
+#[derive(Default)]
+pub enum State {
+    #[default]
+    Listen,
+    Closed,
+    SynRcvd,
+    // SynSent,
+    // Estab,
+}
 
 #[derive(Default)]
-pub struct State;
+pub struct Connection {
+    state: State,
+    send: SendSequenceSpace,
+    recv: ReceiveSequenceSpace,
+}
 
-impl State {
-    pub fn on_packet(&self, ip_header: Ipv4HeaderSlice, tcp_header: TcpHeaderSlice, data: &[u8]) {
-        println!(
-            "{}:{} → {}:{} = {} bytes",
-            ip_header.source_addr(),
-            tcp_header.source_port(),
-            ip_header.destination_addr(),
+#[derive(Default)]
+struct SendSequenceSpace {
+    una: u32,
+    nxt: u32,
+    wnd: u16,
+    iss: u32,
+}
+
+#[derive(Default)]
+struct ReceiveSequenceSpace {
+    nxt: u32,
+    wnd: u16,
+    irs: u32,
+}
+
+impl Connection {
+    pub fn accept(
+        interface: &mut Iface,
+        ip_header: Ipv4HeaderSlice,
+        tcp_header: TcpHeaderSlice,
+    ) -> io::Result<Option<Connection>> {
+        let mut buf = [0u8; 1500];
+
+        if !tcp_header.syn() {
+            // SYN packet expected
+            return Ok(None);
+        }
+
+        let iss = 0;
+        let irs = tcp_header.sequence_number();
+
+        let connection = Connection {
+            state: State::SynRcvd,
+            send: SendSequenceSpace {
+                iss,
+                una: iss,
+                nxt: iss + 1,
+                wnd: 64240,
+            },
+            recv: ReceiveSequenceSpace {
+                irs,
+                nxt: irs + 1,
+                wnd: tcp_header.window_size(),
+            },
+        };
+
+        /*                      TCP header format
+
+         0                   1                   2                   3
+         0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |          Source Port          |       Destination Port        |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |                        Sequence Number                        |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |                    Acknowledgment Number                      |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |  Data |       |C|E|U|A|P|R|S|F|                               |
+        | Offset| Rsrvd |W|C|R|C|S|S|Y|I|            Window             |
+        |       |       |R|E|G|K|H|T|N|N|                               |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |           Checksum            |         Urgent Pointer        |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |                           [Options]                           |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |                                                               :
+        :                             Data                              :
+        :                                                               |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ */
+
+        let mut tcp_header_reply = TcpHeader::new(
             tcp_header.destination_port(),
-            data.len(),
+            tcp_header.source_port(),
+            connection.send.iss,
+            connection.send.wnd,
         );
-        println!();
-        println!("data: {:?}", data);
-        println!();
-        println!();
+
+        tcp_header_reply.syn = true;
+        tcp_header_reply.ack = true;
+        tcp_header_reply.acknowledgment_number = connection.recv.nxt;
+
+        let ip_header_reply = Ipv4Header::new(
+            tcp_header_reply.header_len_u16(),
+            64,
+            IpNumber::TCP,
+            ip_header.destination(),
+            ip_header.source(),
+        )
+        .unwrap();
+
+        tcp_header_reply.checksum = tcp_header_reply
+            .calc_checksum_ipv4(&ip_header_reply, &[])
+            .unwrap();
+
+        let unwritten = {
+            let mut unwritten_buf = &mut buf[..];
+
+            ip_header_reply.write(&mut unwritten_buf)?;
+            tcp_header_reply.write(&mut unwritten_buf)?;
+
+            unwritten_buf.len()
+        };
+
+        let written = buf.len() - unwritten;
+
+        interface.send(&buf[..written])?;
+
+        Ok(Some(connection))
+    }
+
+    #[allow(unused_variables)]
+    pub fn on_packet(
+        &mut self,
+        interface: &mut Iface,
+        ip_header: Ipv4HeaderSlice,
+        tcp_header: TcpHeaderSlice,
+        data: &[u8],
+    ) -> io::Result<()> {
+        print!("{}", String::from_utf8_lossy(data));
+        Ok(())
     }
 }
